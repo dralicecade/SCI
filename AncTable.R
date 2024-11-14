0853c8f94e9a95c342e5b26c09e6b058feb8fbb2
Alice Cade <dralicecade@gmail.com>
2024-11-14 15:35:49 +0000

    Update AncTable

--- AncTable.R
+++ AncTable.R
@@ -8,7 +8,7 @@ ancova_Table <- function(df, changeVal, preVal, Group, name){
   emm <- emmeans(modellm, list(pairwise ~ Group   ))
   ci_s <- confint((emmeans(modellm, list(pairwise ~ Group  ))), level = 0.95)
   
-  df <- data.frame(name = name,  change=c(paste( "F(1, 1) = ", round( model$`F value`[1],2), ", ",round(model$`Pr(>F)`[1],3) )),
+  df <- data.frame(name = name,  change=c(paste( "F(",model$Df[1],", ", model$Df[3],") = ", round( model$`F value`[1],2), ", ",round(model$`Pr(>F)`[1],3) )),
                    meanDiff=c(paste(round(ci_s$`pairwise differences of Group`[1,2],2), " (",round(ci_s$`pairwise differences of Group`[1,5],3),", ", round(ci_s$`pairwise differences of Group`[1,6],3),"), ",
                                     round(summary(emm)$`pairwise differences of Group`[1,6],3))),
                    Group1_EMMEAN=c(paste(round(ci_s$`emmeans of Group`[1,2],2), " (", round(ci_s$`emmeans of Group`[1,5],3),", ",round(ci_s$`emmeans of Group`[1,6],3), ")" )),
